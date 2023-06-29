aeaf38738740ba30ab78a08ff92d231fbe6eb001
legiahuy060903 <huylg147258369@gmail.com>
2023-06-29 09:58:08 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  "no-console": "off", // Tắt cảnh báo về việc sử dụng console.log
+  "no-unused-vars": "off"
+}
